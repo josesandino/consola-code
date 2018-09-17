c3550f93d618ede7019af6ca96c2d3d3b692bcab
Jose Sandino <josesandinonio@gmail.com>
2018-09-17 17:46:41 -0600

    Segundo Commit
    Los div principales para trabajar con el codigo

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
     <title>Consola de Codigo</title>
 </head>
 <body>
-    
+    <div class="texto"></div><!-- Div donde se explica mediante texto --> 
+    <div class="codigo"></div><!--Div donde el usuario escribira su codigo-->
+    <div class="verificado"></div><!--Div donde se imprimirá el resultado del codigo-->
 </body>
 </html>
 

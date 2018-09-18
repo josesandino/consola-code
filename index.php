4ce33cb26ea1cb87d8a17f8fbe0237fb1425eb47
Jose Sandino <josesandinonio@gmail.com>
2018-09-17 19:51:24 -0600

    Cuarto Commit
    Se enlaza el index al css

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Consola de Codigo</title>
+    <link rel="stylesheet" href="css/leccion.css" media="screen">
 </head>
 <body>
     <div class="texto"></div><!-- Div donde se explica mediante texto --> 
